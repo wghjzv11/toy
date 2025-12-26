84397559b7b7a95c23df716b3a9e8b263f95909b
wghjzv11 <sbnmqr00@gmail.com>
2025-12-26 21:39:38 +0900

    작업중
    한 일
    - 주식 티커 DB에 저장 완료
    - 데이터 일부 가져오기 성공.
    해야할 일
    - 가져오지 못한 주식 코드에 대한 데이터를 가져와야 한다.
    - (Further) 데이터 분포 찾아서 분석에 사용할 기간 확인하기

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import re
+
 def to_int(value: str) -> int:
     """
     '+57900'  -> 57900
@@ -5,11 +7,4 @@ def to_int(value: str) -> int:
     '--275'   -> 275
     '53000'   -> 53000
     """
-    value = value.strip()
-    if value.startswith("-"):
-        return int(value[1:])
-    if value.startswith("--"):
-        return int(value[2:])
-    if value.startswith("+"):
-        return int(value[1:])
-    return int(value)
+    return int(re.sub(r'^[+-]+', '', value.strip()))
